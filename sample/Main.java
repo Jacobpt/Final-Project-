5b1339c73ec372f6ba257cdfc020cd531af4a8ac
Jacobpt <50085520+Jacobpt@users.noreply.github.com>
2020-03-29 19:15:12 -0700

    added almost complete project
    removed older version of project and added the version that is essentially complete, aside from implementing Junit

--- sample/Main.java
+++ sample/Main.java
@@ -14,7 +14,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Algorithm Tester");
-        primaryStage.setScene(new Scene(root, 700, 375));
+        primaryStage.setScene(new Scene(root, 620, 375));
         primaryStage.show();
     }
 
